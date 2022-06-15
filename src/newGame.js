4ef0c0199a9a1d519e7f2775ba0eeaebd08212fb
Unyielding1 <david.verde.alvarez@gmail.com>
2022-06-15 16:38:05 -0500

    linters

--- src/newGame.js
+++ src/newGame.js
@@ -1,13 +1,13 @@
 export default async function newGame() {
-    let gameId = [];
-    const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: 'Digitalization' }),
-    });
-    const data = await res.json();
-    gameId = (data.result).split(' ');
-    return gameId[3];
-  }
+  let gameId = [];
+  const res = await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ name: 'Digitalization' }),
+  });
+  const data = await res.json();
+  gameId = (data.result).split(' ');
+  return gameId[3];
+}
--- src/result.js
+++ src/result.js
@@ -1,13 +1,13 @@
 
 class Result {
-    static record(user, score) {
-      return `
+  static record(user, score) {
+    return `
       <div class="result" data-id=${user}>
         <span class="user" data-id=${user}>${user}: </span>
         <span class="score" data-id=${user}>${score}</span>
       </div>
       `;
-    }
   }
-  
-  export default Result;
+}
+
+export default Result;
